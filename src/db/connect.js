ac8a9faf6743afcca8f20915573836a5f445235f
thaydeveloper <thaydeveloper26@gmail.com>
2023-07-11 18:47:21 -0300

    feita alteracao para o mongodb compass

--- src/db/connect.js
+++ src/db/connect.js
@@ -4,8 +4,9 @@ async function main() {
   try {
     mongoose.set("strictQuery", true);
     mongoose.connect(
-      "mongodb+srv://thaydeveloper26:1996@cluster0.dltzmfi.mongodb.net/"
+      "mongodb+srv://thaydeveloper26:1996@cluster0.dltzmfi.mongodb.net/?authMechanism=DEFAULT"
     );
+    console.log("conectouuuu");
   } catch (error) {
     console.log("error:" + error);
   }
